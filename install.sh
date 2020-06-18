0d3f18ef9d73e503b79faaf5309e556b389d2caa
jdvcDev <jason.cook@microsoft.com>
2020-06-17 23:24:36 -0600

    update github actions yaml

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-apt-get update
-apt-get install build-essential
-
+apt-get update -y
+#&& apt-get -y upgrade
+apt-get install build-essential -y
+pip install -r requirements.txt
+touch /tmp/build.out
